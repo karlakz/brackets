a647c0c4947ff24da35c7f3266aee7fdd953300a
karlakz <yakiyaeva@gmail.com>
2019-10-13 15:03:14 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // Store all sets of brackets and their opposites
+    let brackets = {
+        '(':')',
+        '{':'}',
+        '[':']',
+        '|':'|',
+        '1':'2',
+        '3':'4',
+        '5':'6',
+        '7':'7',
+        '8':'8'
+    }
+
+    let inp = str;
+    let stack = [];
+
+    //Check every character in stack
+    for (let i = 0; i < inp.length; i++) {
+        if (brackets[stack[stack.length - 1]] == inp[i]) {
+        stack.pop();
+        } else {
+        stack.push(inp[i]);
+        }
+    }
+
+    return !stack.length;
 }
